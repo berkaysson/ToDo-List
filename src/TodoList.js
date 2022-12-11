a948c225ea58b843902cdcf7e6176bcdd6284a23
berkaysson <berkaysonel85@gmail.com>
2022-12-11 19:42:36 +0300

    Added todolistFactory() to create ToDo list to projects.

---
+++ src/TodoList.js
@@ -0,0 +1,27 @@
+const todolistFactory = () => {
+    let projects = [];
+    
+    const addProject = (newProject) => {
+        projects.push(newProject);
+    }
+
+    const deleteProject = (project) => {
+        projects = projects.filter(_project => _project.getName() !== project);
+    }
+
+    
+    const getProject = (project) => {
+        return projects.find((_project) => _project.getName() === project);
+    }
+
+    const getAllProjects = () => {
+        return projects;
+    }
+
+
+    return {
+        addProject, deleteProject, getProject, getAllProjects
+    }
+}
+
+export default todolistFactory
