0ab5847e6591391bf68d949479a8901e2665c9fe
berkaysson <berkaysonel85@gmail.com>
2022-12-20 19:50:33 +0300

    Made getTodoList() dynamic, it can call todolist and parsed it with a new todolist. Added add, edit ,get project functions.

--- src/Storage.js
+++ src/Storage.js
@@ -9,26 +9,68 @@ export default class Storage  {
     }
 
     static getTodolist() {
-        const _todolist = todolistFactory();
-        const todolist = JSON.parse(localStorage.getItem('todolist'))
-        // console.log("Todolist created")
-
-        for (let i = 0; i < Object.values(todolist)[0].length; i++) {
-            let project = Object.values(todolist)[0][i]
-            let _project = projectFactory(Object.values(project)[1], Object.values(project)[2])
-            // console.log("Project created /*/ " + _project.getName())
-            for (let j=0 ; j<project["tasks"].length;j++){
-                let task = project["tasks"][j]
-                let _task = taskFactory(Object.values(task)[0],Object.values(task)[1],
-                Object.values(task)[2], Object.values(task)[3])
-                // console.log("Task created /*/" + _task.getName())
-                _project.addTask(_task);
-                // console.log("Task added to project /*/" + _task.getName())
-            }
-            // console.log("Project added to todolist /*/ " + _project.getName())
-            _todolist.addProject(_project);
+        if(localStorage.getItem('todolist') === null) {
+            let newTodolist = todolistFactory()
+            let homeProject = projectFactory("Home", "no color")
+            let todayProject = projectFactory("Today", "no color")
+            let thisweekProject = projectFactory("This Week", "no color")
+            newTodolist.addProject(homeProject)
+            newTodolist.addProject(todayProject)
+            newTodolist.addProject(thisweekProject)
+            Storage.setTodolist(newTodolist);
+            return newTodolist;
         }
 
-        return _todolist
+        let todolistParsed = JSON.parse(localStorage.getItem('todolist'))
+        let todolist = todolistFactory();
+        for (let projectKey = 0 ; projectKey<todolistParsed["projects"].length; projectKey++) {
+            let projectParsed = todolistParsed["projects"][projectKey] // it gives project object with tasks: array, name, color but not produced with factory
+            let project = projectFactory(projectParsed.name, projectParsed.color); // it creates new project
+            for(let taskKey = 1; taskKey < projectParsed["tasks"].length; taskKey++) {
+                let taskParsed = project["tasks"][taskKey];
+                let task = taskFactory(taskParsed.name, taskParsed.description,
+                     taskParsed.date, taskParsed.tag);
+                project.addTask(task);
+            }
+            if ((project.name === "Home" || project.name === "Today" || project.name === "This Week") && project.getAllTasks().length>0){
+                todolist.getProject(project.name).addTask(project.getAllTasks())
+            }
+            else {
+                todolist.addProject(project);
+            }
+        }
+        return todolist
+    }
+
+    static getAllProjects(){
+        let todolist = Storage.getTodolist()
+        return todolist.getAllProjects();
+    }
+
+    static getProject(name){
+        let todolist = Storage.getTodolist();
+        let project = todolist.getAllProjects().find(item => item.name === name);
+        return project;
+    }
+
+    static addProject(project) {
+        let todolist = Storage.getTodolist();
+        const newProject = projectFactory(project, "no color");
+        todolist.addProject(newProject);
+        Storage.setTodolist(todolist);
+    }
+
+    static deleteProject(name) {
+        let todolist = Storage.getTodolist();
+        todolist.deleteProject(name);
+        todolist.projects = todolist.getAllProjects()
+        Storage.setTodolist(todolist);
+    }
+
+    static editProject(name, newName){
+        let todolist = Storage.getTodolist();
+        let project = todolist.getAllProjects().find(item => item.name === name);
+        project.name = newName;
+        Storage.setTodolist(todolist)
     }
 }
