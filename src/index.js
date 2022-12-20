423e4ce515d2efdab36a78ed37bedfb3a634ed33
berkaysson <berkaysonel85@gmail.com>
2022-12-20 19:51:31 +0300

    imported all files and called pageRender from UI.js.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import taskFactory from "./Task";
+import projectFactory from "./Project";
+import todolistFactory from "./TodoList";
+import Storage from "./Storage";
+import UI from "./UIController";
 
+UI.pageRender()
